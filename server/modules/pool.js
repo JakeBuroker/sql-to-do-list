5d82279b7c9fcc465a03000c7a976f352266a081
Jake Buroker <shamrecks@gmail.com>
2023-12-17 18:29:18 -0600

    fixed

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,5 +1,5 @@
 const pg = require('pg')
-let databaseName = 'weekend-to-do-app'
+
 if (process.env.DATABASE_URL) {
   pool = new pg.Pool({
       connectionString: process.env.DATABASE_URL,
@@ -12,6 +12,6 @@ const pool = new pg.Pool({
     host: 'localhost',
     port: 5432,
     database: 'weekend-to-do-app',
-    allowExitOnIdle: true 
+
 })
 module.exports = pool
